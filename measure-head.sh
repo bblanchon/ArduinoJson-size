6de079e39b9d55d0f73359ca1519b320a284aec9
Benoit Blanchon <git@benoitblanchon.fr>
2022-01-06 18:40:36 +0100

    Use arduino-cli

--- measure-head.sh
+++ measure-head.sh
@@ -7,16 +7,14 @@ cd "$(dirname "$0")"
 PATH="$PATH:/c/Program Files (x86)/Arduino/"
 BOARD=${1:-arduino:avr:uno}
 REPOSITORY="$(pwd)/libraries/ArduinoJson"
-SKETCHBOOK="$(pwd)"
 EXAMPLES="$REPOSITORY/examples"
 
 compile() {(
-  cd "$(dirname "$1")"
-  arduino --verify --board "$BOARD" --pref "sketchbook.path=$SKETCHBOOK" "$(basename "$1")"
+  arduino-cli compile --libraries "$REPOSITORY" -b "$BOARD" "$1"
 )}
 
 measure_code() {(
-  compile "$1" 2>/dev/null | grep -e 'Sketch uses' | sed -E 's/.*uses ([0-9]*),?([0-9]+).*/\1\2/'
+  compile "$1"  | grep -e 'Sketch uses' | sed -E 's/.*uses ([0-9]*),?([0-9]+).*/\1\2/'
 )}
 
 get_constant() {(
@@ -28,4 +26,4 @@ echo -n "$(git -C "$REPOSITORY" log -1 --date=short --pretty=format:%cd),"
 echo -n "$(measure_code "$EXAMPLES/JsonParserExample/JsonParserExample.ino"),"
 echo -n "$(measure_code "$EXAMPLES/JsonGeneratorExample/JsonGeneratorExample.ino"),"
 echo -n "$(measure_code "$EXAMPLES/MsgPackParser/MsgPackParser.ino"),"
-get_constant "ReferenceObjectSize.ino"
+get_constant "$(pwd)/ReferenceObjectSize/ReferenceObjectSize.ino"
